ffe10c8b95399094aae9375a3bcbeeb897e795a5
EduardoBombonatto <eduardobombonattolorenzetti@gmail.com>
2025-09-29 11:23:31 -0300

    Status

---
+++ Models/Status.java
@@ -0,0 +1,15 @@
+package Models;
+
+public enum Status {
+    TODO("Todo"), IN_PROGRESS("In progress"), DONE("Done");
+
+    private final String value;
+
+    Status(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
